d960763f3ab1682834f7eb8a6fe4d3a6a323c332
chenchong <qinchenchong@gmail.com>
2013-07-23 16:54:58 +0800

    Add the capability of returning multi-rate to the net80211 rate control code. For pre-802.11n, clear rts/cts flag of retries 1, 2, 3. If retry 0 enable rts/cts, blank tries 1, 2, 3. (r254864)

--- ieee80211_ratectl.h
+++ ieee80211_ratectl.h
@@ -22,7 +22,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * $FreeBSD: soc2013/ccqin/head/sys/net80211/ieee80211_ratectl.h 215332 2010-11-14 09:59:52Z bschmidt $
+ * $FreeBSD: soc2013/ccqin/head/sys/net80211/ieee80211_ratectl.h 254864 2013-07-17 03:02:32Z ccqin $
  */
 
 enum ieee80211_ratealgs {
@@ -37,6 +37,25 @@ enum ieee80211_ratealgs {
 #define	IEEE80211_RATECTL_TX_SUCCESS	1
 #define	IEEE80211_RATECTL_TX_FAILURE	0
 
+#define	IEEE80211_RATECTL_NUM		4
+
+#define	IEEE80211_RATECTL_DS_FLAG		0x01	/* dual-stream rate */
+#define	IEEE80211_RATECTL_CW40_FLAG		0x02	/* use HT40 */
+#define	IEEE80211_RATECTL_SGI_FLAG		0x04	/* use short-GI */
+#define	IEEE80211_RATECTL_HT_FLAG		0x08	/* use HT */
+#define	IEEE80211_RATECTL_RTSCTS_FLAG	0x10	/* enable RTS/CTS protection */
+#define	IEEE80211_RATECTL_STBC_FLAG		0x20	/* enable STBC */
+#define	IEEE80211_RATECTL_TS_FLAG		0x40	/* triple-stream rate */
+
+struct ieee80211_rc_series {
+	uint8_t rix;		/* ratetable index, not rate code */
+	uint8_t ratecode;	/* hardware rate code */
+	uint8_t tries;
+	uint8_t tx_power_cap;
+	uint16_t flags;
+	uint16_t max4msframelen;
+};
+
 struct ieee80211_ratectl {
 	const char *ir_name;
 	int	(*ir_attach)(const struct ieee80211vap *);
@@ -46,6 +65,7 @@ struct ieee80211_ratectl {
 	void	(*ir_node_init)(struct ieee80211_node *);
 	void	(*ir_node_deinit)(struct ieee80211_node *);
 	int	(*ir_rate)(struct ieee80211_node *, void *, uint32_t);
+	void	(*ir_rates)(struct ieee80211_node *, struct ieee80211_rc_series *, int, size_t);
 	void	(*ir_tx_complete)(const struct ieee80211vap *,
 	    			  const struct ieee80211_node *, int,
 	    			  void *, void *);
@@ -90,6 +110,26 @@ ieee80211_ratectl_rate(struct ieee80211_node *ni, void *arg, uint32_t iarg)
 	const struct ieee80211vap *vap = ni->ni_vap;
 
 	return vap->iv_rate->ir_rate(ni, arg, iarg);
+}
+
+static void __inline
+ieee80211_ratectl_rates(struct ieee80211_node *ni, struct ieee80211_rc_series *rc,
+		int shortPreamble, size_t frameLen)
+{
+	const struct ieee80211vap *vap = ni->ni_vap;
+	struct ieee80211com *ic = vap->iv_ic; 
+
+	vap->iv_rate->ir_rates(ni, rc, shortPreamble, frameLen);
+
+	/* if enable rts/cts and is pre-802.11n, blank tries 1, 2, 3 */
+	if (!(ic->ic_htcaps & IEEE80211_HTC_HT))
+	{
+		if (rc[0].flags & IEEE80211_RATECTL_RTSCTS_FLAG)
+			rc[1].tries = rc[2].tries = rc[3].tries = 0;
+		rc[1].flags &= ~IEEE80211_RATECTL_RTSCTS_FLAG; 
+		rc[2].flags &= ~IEEE80211_RATECTL_RTSCTS_FLAG; 
+		rc[3].flags &= ~IEEE80211_RATECTL_RTSCTS_FLAG; 
+	}
 }
 
 static void __inline
